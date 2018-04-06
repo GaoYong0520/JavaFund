a492004709b557c3e13bc2e8ba57938069b1d068
MSER <gaoyong0520@outlook.com>
2018-04-06 15:34:01 +0800

    Single Object

--- src/singleobj/Main.java
+++ src/singleobj/Main.java
@@ -1,7 +1,13 @@
 package singleobj;
 
+import java.security.Signature;
+
 class Main {
     public static void main(String[] args) {
+        //错误示例
+        //Signature object = new SingleObject();
+
+        //获取唯一可用对象
         SingleObject instance = SingleObject.getInstance();
 
         instance.sayHello();
--- src/singleobj/SingleObject.java
+++ src/singleobj/SingleObject.java
@@ -1,10 +1,13 @@
 package singleobj;
 
 class SingleObject {
+    //创建SingleObject的一个对象
     private static SingleObject instance = new SingleObject();
 
+    //构造函数私有化，该类不会被实例化
     private SingleObject() {}
 
+    //获取唯一可用对象
     public static SingleObject getInstance() {
         return instance;
     }
